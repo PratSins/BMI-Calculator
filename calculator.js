a3511cfe2ff297772661057e14d06ebd6a45afb7
PratSins <pratyush2002ps@gmail.com>
2023-08-05 16:09:14 +0530

    Added the proj

---
+++ calculator.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+// app.use(bodyParser)
+// app.use(bodyParser.text())
+// app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/index.html");
+})
+
+app.post("/", function(req, res){
+    // console.log(req.body)
+    // console.log(req.body.num1)
+    var x = Number(req.body.num1)
+    var y = Number(req.body.num2)
+    var z = x+y
+
+    res.send("Sum = "+z);
+})
+
+app.get("/bmiCalc",function(req, res){
+    res.sendFile(__dirname + "/bmi.html");
+})
+
+app.post("/bmiCalc",function(req, res){
+    var w = Number(req.body.wt)
+    var h = Number(req.body.ht)
+    var z = w/(h*h)
+
+    res.send("BMI = "+z);
+})
+app.listen(3000,()=>console.log('HelloWorld App running on port 3000'))
---
+++ readme.txt
@@ -0,0 +1,11 @@
+npm init
+npm install express
+
+node server.js
+
+npm install -g nodemon
+
+---- delete the file nodemon looks like notepad a bit at C:\Users\Pratyush\AppData\Roaming\npm
+nodemon server.js
+
+npm install body-parser
